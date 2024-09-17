57e742b8c6d8586f6f6b08f70bd4d20c1170f4e9
Oriol Canadés <oriol.canades@in2.es>
2024-09-17 09:45:54 +0200

    New project Update Pipelines

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// This test is empty because it is just to check if the context loads
+	}
+
+}
